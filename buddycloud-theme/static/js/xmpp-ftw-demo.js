63cb6522d9358c6021d5c24bb41b021419dfd57a
Guilherme Baptista <guisgb13@gmail.com>
2014-01-29 21:04:25 +0100

    Fixed xmpp.login.anonymous arguments

--- buddycloud-theme/static/js/xmpp-ftw-demo.js
+++ buddycloud-theme/static/js/xmpp-ftw-demo.js
@@ -278,7 +278,7 @@ $(window.document).ready(function() {
     socket.on('online', function(data) {
         console.log('Connected', data)
 	console.log("sent anonymous login");
-        socket.send('xmpp.login.anonymous', {})
+        socket.send('xmpp.login.anonymous', { jid: '@anon.buddycloud.org' })
 	console.log("sent connected");
         socket.on('xmpp.connected', function() {
             socket.send('xmpp.buddycloud.discover', {}, function(error, data) {
